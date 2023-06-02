ec0d846065b483c9648485184440ccea9a672896
Joshua Coffey <itsjoshcoffey@gmail.com>
2023-06-01 22:15:30 -0500

    a little lost got the server wired up like we did in class but do not know where to go from here

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ const messageQueue = new Queue;
 
 const server = new Server();
 
+server.listen(PORT);
+
 const caps = server.of('/caps');
 
 function logger(event, payload) {
@@ -15,28 +17,72 @@ function logger(event, payload) {
   console.log('EVENT: ', { event, timestamp, payload });
 }
 
-server.on('connection', (socket) => {
-  console.log('Server socket connection to event server: ', socket.id);
-});
-
 caps.on('connection', (socket) => {
-  console.log('Caps socket connection to server: ', socket.id);
+  console.log('Server socket connection to event server: ', socket.id);
+  socket.onAny((event, payload) => {
+    logger(event, payload);
+  });
 
   socket.on('pickup', (payload) => {
-    logger('pickup', payload);
+    let currentQueue = messageQueue.read('DRIVER');
+    if(!currentQueue){
+      let queueKey = messageQueue.store('DRIVER', new Queue());
+      currentQueue = messageQueue.read(queueKey);
+    }
+    currentQueue.store(payload.messageId, payload);
     socket.broadcast.emit('pickup', payload);
   });
 
+  socket.on('received', (payload) => {
+    let currentQueue = messageQueue.read(payload.queueId);
+    if(!currentQueue) {
+      throw new Error('we have a message but no queue');
+    }
+    let order = currentQueue.remove(payload.messageId);
+    socket.broadcast.emit('received', order);
+  });
+
   socket.on('in-transit', (payload) => {
-    logger('in-transit', payload);
     socket.broadcast.emit('in-transit', payload);
   });
 
   socket.on('delivered', (payload) => {
-    logger('delivered', payload);
+    let currentQueue = messageQueue.read(payload.store);
+    if(!currentQueue) {
+      let queueKey = messageQueue.store(payload.store, new Queue());
+      currentQueue = messageQueue.read(queueKey);
+    }
+    currentQueue.store(payload.store, payload);
     socket.broadcast.emit('delivered', payload);
   });
 
+  socket.on('getAll', (payload) => {
+    let currentQueue = messageQueue.read(payload.queueId);
+    if(currentQueue && currentQueue.data) {
+      Object.keys(currentQueue.data).forEach(orderId => {
+        socket.emit('pickup', currentQueue.read(orderId));
+      });
+    }
+  });
 });
 
-server.listen(PORT);
+// caps.on('connection', (socket) => {
+//   console.log('Caps socket connection to server: ', socket.id);
+
+//   socket.on('pickup', (payload) => {
+//     logger('pickup', payload);
+//     socket.broadcast.emit('pickup', payload);
+//   });
+
+//   socket.on('in-transit', (payload) => {
+//     logger('in-transit', payload);
+//     socket.broadcast.emit('in-transit', payload);
+//   });
+
+//   socket.on('delivered', (payload) => {
+//     logger('delivered', payload);
+//     socket.broadcast.emit('delivered', payload);
+//   });
+
+// });
+
